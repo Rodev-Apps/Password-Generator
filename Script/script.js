b1419934845a4a9c8174512eb3b8328a8e0da734
SRGit303 <superod@outlook.com>
2023-01-17 16:10:52 +0000

    Setup user prompt and alert

--- Script/script.js
+++ Script/script.js
@@ -88,10 +88,36 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+var userLength
+var lowerChoice
+var upperChoice
+var numChoice
+var specialChoice
+var colatePass = []
+
+
 // Function to prompt user for password options
 function getPasswordOptions() {
-
+  userLength = prompt("Enter the length of the password required");
+  if (userLength < 8 || userLength > 64) {
+    alert("Passwords must be between 8 - 64 characters in length.");
+    
+  } else if (userLength >= 8 || userLength <= 64) {
+      lowerChoice = confirm("Click OK to include lowercase letters"); 
+      upperChoice = confirm("Click OK to include uppercase letters");
+      numChoice = confirm("Click OK to include numbers");
+      specialChoice = confirm("Click OK to include special characters"); 
+     
+   
+    if (lowerChoice === false, upperChoice === false, numChoice === false, specialChoice === false) {
+      alert("You must accept at least one option; please try again") 
+      } else {
+      alert("THANKY YOU for using Password Generator")
+    }
+  }  console.log(userLength, lowerChoice, upperChoice, numChoice, specialChoice)
 }
+
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -116,3 +142,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+getPasswordOptions()
